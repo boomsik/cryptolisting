5a19b9d840e47d8190ebd7ba8fa0a26f3eb24bea
boomsik <pozdnyakov151994@gmail.com>
2023-12-18 00:15:53 +0200

    add pagination

--- src/Components/ColectItem/ColectItem.jsx
+++ src/Components/ColectItem/ColectItem.jsx
@@ -1,15 +1,53 @@
 import ColectDiv from "../ColectDiv/ColectDiv";
-
+import ReactPaginate from "react-paginate";
+import { React, useEffect, useState } from "react";
 import "./ColectItem.css";
 function ColectItem({ collection, del }) {
+    const data = collection;
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 10;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, data]);
+
+    const handlePageClick = (e) => {
+        const newOffset = (e.selected * itemsPerPage) % data.length;
+        setItemOffset(newOffset);
+    };
     return (
-        <div className="containers">
-            <ul className="list">
-                {collection.map((el) => (
-                    <ColectDiv key={el.id} collection={el} del={del} />
-                ))}
-            </ul>
-        </div>
+        <>
+            <div className="containers">
+                <ul className="list">
+                    {currentItems.map((el) => (
+                        <ColectDiv key={el.id} collection={el} del={del} />
+                    ))}
+                </ul>
+                <ReactPaginate
+                    breakLabel="....."
+                    nextLabel=" >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    pageCount={pageCount}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                    containerClassName="pagination"
+                    pageClassName="pagination_item"
+                    previousClassName="pagination_item_tag"
+                    nextClassName="pagination_item_next"
+                    pageLinkClassName="page-num"
+                    previousLinkClassName="page-num"
+                    nextLinkClassName="page-num"
+                    activeLinkClassName="active"
+                    activeClassName="active_li"
+                    breakClassName="free_dot"
+                />
+            </div>
+        </>
     );
 }
 
